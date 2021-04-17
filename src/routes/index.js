761fe7abbd38d13429848e3100a0e1a2b6ba9ce3
shayan <ssshayanss777@gmail.com>
2021-04-17 17:54:36 +0430

    Build UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,14 @@ router.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+router.get('/auth', (req, res) => {
+    res.redirect('/');
+});
+
+router.get('/chat', (req, res) => {
+    res.redirect('/');
+});
+
 router.use('/api/auth', authRoutes);
 
 module.exports = router;
